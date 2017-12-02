0d045002e4251e1edc75de57ddfd3a1af7f40447
Tobias Wirén <tobias.wiren@exsitec.se>
2017-12-02 17:14:47 +0100

    Solution for step 2

--- 2/step2.js
+++ 2/step2.js
@@ -10,19 +10,20 @@ fs.readFile('input', 'utf8', function(err, data) {
   solve(data);
 });
 
+function combinations(row) {
+  return row.reduce((acc, v, ix) =>
+      acc.concat(row.slice(ix + 1).map(x => [ [v, x], [x, v] ]))
+    , [])
+    .reduce((a, b) => a.concat(b));
+}
+
 function solve(input) {
   var res = input.split('\n')
     .filter(s => s != '')
     .map(s => s.split(/[ \t]+/))
-    .map(row => minmax(row))
-    .reduce((sum, mm) => sum + mm[1] - mm[0], 0);
+    .map(row => combinations(row))
+    .map(arr => arr.filter(pair => pair[0] % pair[1] == 0).map(p => p[0] / p[1]))
+    .reduce((sum, x) => sum + x[0], 0);
 
   console.log(res);
-}
-
-function minmax(row) {
-  return row.map(c => Number(c))
-    .reduce((arr, x) => {
-      return [Math.min(x, arr[0]), Math.max(x, arr[1])];
-    }, [Number.MAX_SAFE_INTEGER, 0]);
 }
