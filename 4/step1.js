c6a1c869fca05a2237c7d51f381b067ea20cb0fe
Tobias Wirén <tobias.wiren@exsitec.se>
2017-12-04 09:57:04 +0100

    solution for 4/12 step 1

---
+++ 4/step1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var fs = require('fs');
+
+fs.readFile('input', 'utf8', function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  solve(data);
+});
+
+function isValid(pwd) {
+  return pwd.split(' ').reduce((eq, w, ix, arr) =>
+    eq + arr.slice(ix + 1).reduce((nrEq, w2) => nrEq + (w == w2 ? 1 : 0), 0)
+  , 0) == 0;
+}
+
+function solve(data) {
+  const nrValid = data.split('\n').filter(x => x != '')
+    .reduce((sum, phrase) => sum + (isValid(phrase) ? 1 : 0), 0);
+  console.log(nrValid);
+}
+
+solve("aa bb cc dd ee");
+solve("aa bb cc dd aa bb");
+solve("aa bb cc dd aaa");
