b653baeaaca1d420d6d30dc27feddaddb80ceafc
Tobias Wirén <tobias.wiren@exsitec.se>
2017-12-05 08:32:59 +0100

    solution for day 5, step 2

---
+++ 5/step2.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// run with node --harmony step2.js
+
+var fs = require('fs');
+
+fs.readFile('input', 'utf8', function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  solve(data.split('\n').filter(x => x != '').map(x => Number(x)));
+});
+
+function step(n, ix, inst) {
+  if (ix >= inst.length) return n;
+  const nextIx = ix + inst[ix];
+  inst[ix] = inst[ix] + (inst[ix] > 2 ? -1 : 1);
+  return step(n + 1, nextIx, inst);
+}
+
+function solve(data) {
+  const res = step(0, 0, data);
+  console.log("It takes " + res + " steps to escape the maze");
+}
+
+solve([0, 3, 0, 1, -3]);
